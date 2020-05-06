cc695dd86666ddab15a4dc18089c7ce1de62112f
Nico <nikolaasrondon@gmail.com>
2020-05-06 01:39:32 -0500

    Fix test new profile

--- core/tests/accounts/test_authentication.py
+++ core/tests/accounts/test_authentication.py
@@ -62,8 +62,8 @@ class UsersMethodsTestCase(SetupBase):
 class NewProfileTestCase(SetupBase):
 
     # Test que prueba que un perfil se crea después de crear un usuario
-    def test_new_article(self):
+    def test_new_profile(self):
         auth = self.client.post('/api/token/', {'email': 'test_user@mail.com', 'password': '123456'})
         token = auth.data['access']
-        result = self.client.get('/api/v1/profiles/1/', HTTP_AUTHORIZATION='Bearer {0}'.format(token))
+        result = self.client.get('/api/v1/profiles/test_user/', HTTP_AUTHORIZATION='Bearer {0}'.format(token))
         assert result.status_code == 200
